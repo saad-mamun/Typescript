3f5b8ae05c8ac285a22668f8442e4c4541d06d40
saad-mamun <mamunali88991@gmail.com>
2024-12-18 21:24:54 +0600

    first class

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+let isTrue = true;
+let age = 30;
+let fullName = "Liton";
+fullName = "Mamun";
+let lastName = "Al-Mamun";
+lastName = 198;
+const names = [];
+names.push("Liton");
+const aj = ["liton", "mamun"];
+let myTuple;
+myTuple = [4, true, "Liton Vaiya"];
+console.log(myTuple);
